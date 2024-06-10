6d937efeca3a3759f2fb32ba1523f3851223fc1e
HadoukenW <hadouken22338@gmail.com>
2024-06-11 00:11:12 +0500

    db connection

--- client/src/pages/ErrorPage.js
+++ client/src/pages/ErrorPage.js
@@ -30,7 +30,7 @@ const ErrorPage = () => {
               {isAuthenticated ? (
                 <Link to="/">
                   <button className="text-2xl sm:w-full lg:w-auto my-2 border rounded-md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
-                    Вернуться на главную
+                    Вернуться на главную страницу
                   </button>
                 </Link>
               ) : (
